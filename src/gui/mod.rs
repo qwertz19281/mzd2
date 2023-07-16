b1bea5a03a148a248e2bedea32887d6cada6b329
qwertz19281 <dellen903@gmail.com>
2023-07-16 20:17:41 +0200

    just apply dpi to the entire style instead

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::VecDeque;
+use std::sync::Arc;
 
 use egui::Vec2;
 
@@ -69,8 +70,80 @@ pub fn dpi_hack(ctx: &egui::Context, frame: &mut eframe::Frame) -> f32 {
         font.tweak.scale *= dpi;
     }
 
+    let mut style = ctx.style();
+
+    fn marge(s: &mut egui::Margin, dpi: f32) {
+        s.left *= dpi;
+        s.right *= dpi;
+        s.top *= dpi;
+        s.bottom *= dpi;
+    }
+
+    fn rond(s: &mut egui::Rounding, dpi: f32) {
+        s.nw *= dpi;
+        s.ne *= dpi;
+        s.sw *= dpi;
+        s.se *= dpi;
+    }
+
+    {
+        let style = Arc::make_mut(&mut style);
+        
+        {
+            let s = &mut style.spacing;
+            s.button_padding *= dpi;
+            s.combo_height *= dpi;
+            s.combo_width *= dpi;
+            s.icon_spacing *= dpi;
+            s.icon_width *= dpi;
+            s.icon_width_inner *= dpi;
+            s.indent *= dpi;
+            s.interact_size *= dpi;
+            s.item_spacing *= dpi;
+            marge(&mut s.menu_margin, dpi);
+            s.scroll_bar_inner_margin *= dpi;
+            s.scroll_bar_outer_margin *= dpi;
+            s.scroll_bar_width *= dpi;
+            s.scroll_handle_min_length *= dpi;
+            s.slider_width *= dpi;
+            s.text_edit_width *= dpi;
+            s.tooltip_width *= dpi;
+            s.tooltip_width *= dpi;
+            marge(&mut s.window_margin, dpi);
+        }
+        {
+            let s = &mut style.visuals;
+            s.clip_rect_margin *= dpi;
+            rond(&mut s.menu_rounding, dpi);
+            s.popup_shadow.extrusion *= dpi;
+            s.resize_corner_size *= dpi;
+            s.selection.stroke.width *= dpi;
+            s.text_cursor_width *= dpi;
+            rond(&mut s.window_rounding, dpi);
+            s.window_shadow.extrusion *= dpi;
+            s.window_stroke.width *= dpi;
+        }
+        {
+            let s = &mut style.visuals.widgets;
+            
+            fn wv(s: &mut egui::style::WidgetVisuals, dpi: f32) {
+                s.bg_stroke.width *= dpi;
+                s.expansion *= dpi;
+                s.fg_stroke.width *= dpi;
+                rond(&mut s.rounding, dpi);
+            }
+
+            wv(&mut s.active, dpi);
+            wv(&mut s.hovered, dpi);
+            wv(&mut s.inactive, dpi);
+            wv(&mut s.noninteractive, dpi);
+            wv(&mut s.open, dpi);
+        }
+    }
+
     ctx.set_pixels_per_point(1.);
     ctx.set_fonts(fontdef);
+    ctx.set_style(style);
 
     dpi
 }
